14d8f282d04b86591ebe7bab4c60545d5b77782a
Thomas Veit <thomas.veit93@gmail.com>
2021-07-14 21:10:35 +0100

    home page cards layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   const fetchItems = async () => {
     const data = await fetch("https://api.pokemontcg.io/v2/sets?q=series:base");
     const items = await data.json();
+    console.log(items.data);
     // set state
     setItems(items.data);
   };
@@ -18,7 +19,14 @@ const Home = () => {
   return (
     <div className="home">
       {items.map(item => (
-        <img key={item.ptcgoCode} src={item.images.logo} alt={item.name}/>
+        <div className="set-container" key={item.ptcgoCode}>
+          <img src={item.images.logo} alt={item.name}/>
+          <div>
+            <img src={item.images.symbol} alt="symbol"/>
+            <p className="set-name">{item.name}</p>
+            <p className="set-release-date">Released in {item.releaseDate}</p>
+          </div>
+        </div>
       ))}
     </div>
   )
