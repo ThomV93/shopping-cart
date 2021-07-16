809bf6c356f56e85c13d310282ce0b79859466d5
Thomas Veit <thomas.veit93@gmail.com>
2021-07-16 18:55:25 +0100

    home layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./home.scss";
+import pokeball from "./pokeball.png";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="text-container">
+        <h1>Welcome to Poké Antiques</h1>
+        <h3>Join our nostalgic community dedicated to the first Pokémon TCG sets</h3>
+        <div>
+          <p>Created by Thomas Veit</p>
+          <p>built with Pokémon TCG API</p>
+        </div>
+      </div>
+      <div className="image-container">
+        <img src={pokeball} alt="Onix" />
+      </div>
     </div>
   )
 };
